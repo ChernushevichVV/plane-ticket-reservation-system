b2aa5a1454ac36f83e7768051ac3f387c8e1ae4f
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-04 16:11:39 +0300

    update main page styles

--- src/client/components/pages/mainpage/FlightInputForm.js
+++ src/client/components/pages/mainpage/FlightInputForm.js
@@ -4,7 +4,6 @@ export class FlightInputForm extends Component {
   render() {
     return (
       <div className="flight-info">
-        <p>FlightInputForm</p>
         <DatePickerBlock />
       </div>
     );
