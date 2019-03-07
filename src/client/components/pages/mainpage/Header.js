89b11c7bb2ef32d518654318745ed0bb61174b14
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-07 13:54:56 +0300

    add signing buttont to header

--- src/client/components/pages/mainpage/Header.js
+++ src/client/components/pages/mainpage/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 export class Header extends Component {
   render() {
-    return <div className="header" />;
+    return (
+      <div className="header">
+        <div class="sign-block border">
+          <div class="sign-block__button ">
+            <button>Sign up</button>
+          </div>
+          <div class="sign-block__button ">
+            <button>Log in</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
