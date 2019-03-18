153c571c8b11b2ae87e4e7a66009252220161da3
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-18 17:58:39 +0300

    initialize router

--- src/client/components/pages/mainpage/Section.js
+++ src/client/components/pages/mainpage/Section.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import FlightInfoForm from "./FlightInfoForm";
+import BookingBar from "../bookingpage/BookingBar";
+import FlightPicker from "../bookingpage/FlightPicker";
+import { Route } from "react-router-dom";
 
 export class Section extends Component {
   render() {
     return (
       <div className="section">
-        <FlightInfoForm />
+        <Route path="/booking" component={BookingBar} />
+        {/* there should be 1 or 2 pickers depends on tripType
+        //  and page should be responsive to their height
+        */}
+        <Route path="/booking" component={FlightPicker} />
+
+        <Route exact path="/" component={FlightInfoForm} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./client/store";
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
