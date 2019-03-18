80338e691482541b37d6e0fd1afc637216a38bc0
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-18 15:10:31 +0300

    add redirection to booking page

--- src/client/components/pages/mainpage/FlightInfoForm.js
+++ src/client/components/pages/mainpage/FlightInfoForm.js
@@ -3,9 +3,22 @@ import DatePickerBlock from "./DatePickerBlock";
 import OneWayReturnChoice from "./OneWayReturnChoice";
 import DestinationBlock from "./DestinationBlock";
 import Stepper from "./Stepper";
+import { Redirect } from "react-router";
 
 export class FlightInfoForm extends Component {
+  state = {
+    redirect: false
+  };
+
+  handleClick = () => {
+    this.setState({ redirect: true });
+  };
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect push to="/booking" />;
+    }
+
     return (
       <div className="flight-info">
         <OneWayReturnChoice />
@@ -15,7 +28,9 @@ export class FlightInfoForm extends Component {
           <Stepper label="Adult(18+)" minValue={1} />
           <Stepper label="Child(0-18)" minValue={0} />
         </div>
-        <button className="search-button button">Search</button>
+        <button onClick={this.handleClick} className="search-button button">
+          Search
+        </button>
       </div>
     );
   }
