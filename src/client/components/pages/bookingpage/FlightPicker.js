1414a2a5a247335a147d6fd21881473efa0491ce
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-20 18:42:24 +0300

    add total cost field and next button

--- src/client/components/pages/bookingpage/FlightPicker.js
+++ src/client/components/pages/bookingpage/FlightPicker.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import FlightDetailsBlock from "./FlightDetailsBlock";
+import DateCarousel from "./DateCarousel";
+import TotalCost from "../../misc/TotalCost";
 
 export class FlightPicker extends Component {
   render() {
     return (
       <div className="flight-picker">
-        {/* date bar should be here */}
-
+        <DateCarousel />
         {/* the nubmer of these componenets == nubmer of flights */}
         <FlightDetailsBlock />
+        <FlightDetailsBlock />
+
+        <div className="flight-picker__cost-button-block">
+          <TotalCost cost="100$" />
+          <button className="flight-picker__button button">Next</button>
+        </div>
       </div>
     );
   }
