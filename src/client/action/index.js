d3aaac32648ec712d8965a4643cc503e76265d23
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-25 19:29:15 +0300

    add actions

--- src/client/action/index.js
+++ src/client/action/index.js
@@ -3,3 +3,11 @@ import { actionTypes } from "./actionTypes";
 export function changeTripType(payload) {
   return { type: actionTypes.TRIP_TYPE_OPTION_CHANGE, payload };
 }
+
+export function changeDepartureAirport(payload) {
+  return { type: actionTypes.DEPARTURE_AIRPORT_CHANGE, payload };
+}
+
+export function changeDestinationAirport(payload) {
+  return { type: actionTypes.DESTINATION_AIRPORT_CHANGE, payload };
+}
