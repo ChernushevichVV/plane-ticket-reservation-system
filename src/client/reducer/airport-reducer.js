ff84faa31a3d4edf91ff8ff649a37414e88b4fd8
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-26 19:30:08 +0300

    fix bug with airport object

--- src/client/reducer/airport-reducer.js
+++ src/client/reducer/airport-reducer.js
@@ -2,10 +2,10 @@ import { actionTypes } from "../action/actionTypes";
 
 const airportChange = (state = {}, action) => {
   if (action.type === actionTypes.DEPARTURE_AIRPORT_CHANGE) {
-    return { airport: { ...state.airport, departure: action.payload } };
+    return { ...state, departure: action.payload };
   }
   if (action.type === actionTypes.DESTINATION_AIRPORT_CHANGE) {
-    return { airport: { ...state.airport, destination: action.payload } };
+    return { ...state, destination: action.payload };
   }
   return state;
 };
