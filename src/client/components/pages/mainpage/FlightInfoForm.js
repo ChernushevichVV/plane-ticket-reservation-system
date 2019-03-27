d070567bd190404748cf16143bc0a5f4492fdcd1
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-27 14:31:20 +0300

    update stepper logic

--- src/client/components/pages/mainpage/FlightInfoForm.js
+++ src/client/components/pages/mainpage/FlightInfoForm.js
@@ -2,20 +2,17 @@ import React, { Component } from "react";
 import DatePickerBlock from "./DatePickerBlock";
 import OneWayReturnChoice from "./OneWayReturnChoice";
 import AirportPickerBlock from "./AirportPickerBlock";
-import Stepper from "./Stepper";
+import StepperBlock from "./StepperBlock";
 import { Link } from "react-router-dom";
 
-export class FlightInfoForm extends Component {
+class FlightInfoForm extends Component {
   render() {
     return (
       <div className="flight-info">
         <OneWayReturnChoice />
         <AirportPickerBlock />
         <DatePickerBlock />
-        <div className="content">
-          <Stepper label="Adult(18+)" minValue={1} />
-          <Stepper label="Child(0-18)" minValue={0} />
-        </div>
+        <StepperBlock />
         <button className="search-button button">
           <Link to="/booking" className="search-button__link">
             Search
---
+++ src/client/components/pages/mainpage/StepperBlock.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Stepper from "./Stepper";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addPassenger } from "../../../action/index";
+import { deletePassenger } from "../../../action/index";
+
+class ConnectedStepperBlock extends Component {
+  handleDecrement = e => {
+    const type = e.target.id;
+    this.props.deletePassenger(type);
+  };
+
+  handleIncrement = e => {
+    const type = e.target.id;
+    this.props.addPassenger(type);
+  };
+
+  render() {
+    return (
+      <div className="content">
+        <Stepper
+          label="Adult(18+)"
+          id="adult"
+          minValue={1}
+          maxValue={6}
+          num={this.props.passenger.adult}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+        />
+        <Stepper
+          label="Child(0-18)"
+          id="child"
+          minValue={0}
+          maxValue={6}
+          num={this.props.passenger.child}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+        />
+      </div>
+    );
+  }
+}
+
+ConnectedStepperBlock.propTypes = {
+  passenger: PropTypes.object,
+  addPassenger: PropTypes.func,
+  deletePassenger: PropTypes.func
+};
+
+const mapStateToProps = state => {
+  return { passenger: state.passenger };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addPassenger: type => {
+      dispatch(addPassenger(type));
+    },
+    deletePassenger: type => {
+      dispatch(deletePassenger(type));
+    }
+  };
+};
+
+const StepperBlock = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ConnectedStepperBlock);
+
+export default StepperBlock;
