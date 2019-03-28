2e8577be120829469075b736546d247c63bf29c9
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-03-28 15:54:14 +0300

    add reusable Next button

---
+++ src/client/components/misc/NextButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const NextButton = props => {
+  return (
+    <div>
+      <button className="next-button">Next</button>
+    </div>
+  );
+};
+
+NextButton.propTypes = {};
+
+export default NextButton;
