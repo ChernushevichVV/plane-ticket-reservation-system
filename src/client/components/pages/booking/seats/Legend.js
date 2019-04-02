ad009c08f9fd508c8c8d53d37133a33d306c1b05
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-04-02 17:03:06 +0300

    update legend

--- src/client/components/pages/booking/seats/Legend.js
+++ src/client/components/pages/booking/seats/Legend.js
@@ -1,30 +1,30 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Seat from "./Seat";
+//import PropTypes from "prop-types";
+//import Seat from "./Seat";
 
-const Legend = props => {
+const Legend = () => {
   return (
     <div className="legend">
       Seat information
       <hr />
       <div className="legend__seat">
-        <Seat status="available" />
+        <div className="seat" />
         <span className="legend__span"> - available</span>
       </div>
       <hr />
       <div className="legend__seat">
-        <Seat status="reserved" />
+        <div className="seat seat_reserved" />
         <span className="legend__span"> - reserved</span>
       </div>
       <hr />
       <div className="legend__seat">
-        <Seat status="selected" />
+        <div className="seat seat_selected" />
         <span className="legend__span"> - selected</span>
       </div>
     </div>
   );
 };
 
-Legend.propTypes = {};
+//Legend.propTypes = {};
 
 export default Legend;
