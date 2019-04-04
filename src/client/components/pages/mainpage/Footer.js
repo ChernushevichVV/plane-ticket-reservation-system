37b683d397aad6cb142dbe42d8e7b4efcdb025fa
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-04-04 20:29:46 +0300

    now footer looks not that bad as it was

--- src/client/components/pages/mainpage/Footer.js
+++ src/client/components/pages/mainpage/Footer.js
@@ -4,6 +4,26 @@ export class Footer extends Component {
   render() {
     return (
       <div className="footer">
+        <ul className="footer__list">
+          <li className="footer__list-item">
+            <a href="#">Contacts</a>
+          </li>
+          <li className="footer__list-item">
+            <a href="#">About</a>
+          </li>
+          <li className="footer__list-item">
+            <a href="#">Help</a>
+          </li>
+          <li className="footer__list-item">
+            <a href="#">Privacy policy</a>
+          </li>
+          <li className="footer__list-item">
+            <a href="#">Terms of service</a>
+          </li>
+        </ul>
+        <p className="footer__text">
+          ©2019-2019 FlyInTheSky. All rights reserved.
+        </p>
       </div>
     );
   }
