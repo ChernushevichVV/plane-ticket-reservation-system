954c6d2a88343e9b6b5c716742080cb029be1a44
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-04-04 13:02:03 +0300

    add redux

--- src/client/components/pages/booking/seats/Seat.js
+++ src/client/components/pages/booking/seats/Seat.js
@@ -1,32 +1,74 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
+import { connect } from "react-redux";
+import { changeSeatSelection } from "../../../../action/index";
 
-export default class Seat extends Component {
+class ConnectedSeat extends Component {
   state = {
     selected: false
   };
 
   handleClick = () => {
-    this.setState(state => {
-      console.log("click!" + this.state.selected);
+    let { seats, passenger } = this.props;
 
-      return { selected: !state.selected };
+    //don't handle click if all posible seats was already chosen
+    if (
+      seats.length === passenger.adult + passenger.child &&
+      !this.state.selected
+    ) {
+      return;
+    }
+
+    this.setState(state => {
+      return {
+        selected: !state.selected
+      };
     });
+
+    !this.state.selected
+      ? seats.push(this.props.id)
+      : seats.splice(seats.indexOf(this.props.id), 1);
+    this.props.changeSeatSelection(seats);
   };
 
   render() {
+    const { id, status } = this.props;
+
     const style = classNames({
       seat: true,
-      seat_reserved: this.props.status === "reserved",
+      "none-pointer": status === "reserved",
+      seat_reserved: status === "reserved",
       seat_selected: this.state.selected
     });
 
-    return <div className={style} onClick={this.handleClick} />;
+    return <div className={style} onClick={this.handleClick} id={id} />;
   }
 }
 
-Seat.propTypes = {
+ConnectedSeat.propTypes = {
   status: PropTypes.string,
-  id: PropTypes.string
+  id: PropTypes.string,
+  seats: PropTypes.array,
+  changeSeatSelection: PropTypes.func,
+  passenger: PropTypes.object
 };
+
+const mapStateToProps = state => {
+  return { passenger: state.passenger, seats: state.seats };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    changeSeatSelection: seats => {
+      dispatch(changeSeatSelection(seats));
+    }
+  };
+};
+
+const Seat = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ConnectedSeat);
+
+export default Seat;
