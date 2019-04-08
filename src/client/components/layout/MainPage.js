410c65e7ba8189877026c3e28db97d5c3afb40cb
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-04-08 11:54:06 +0300

    add redirection to user page

--- src/client/components/layout/MainPage.js
+++ src/client/components/layout/MainPage.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import Header from "../pages/mainpage/Header";
 import Section from "../pages/mainpage/Section";
 import Footer from "../pages/mainpage/Footer";
+import { Route, Switch } from "react-router-dom";
+import User from "../pages/accounts/User";
 
 export class MainPage extends Component {
   render() {
     return (
       <div className="container">
         <Header />
-        <Section />
+        <Switch>
+          <Route path="/user" component={User} />
+          <Route path="/" component={Section} />
+        </Switch>
         <Footer />
       </div>
     );
