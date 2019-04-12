f4fa2194c2333a099d82cb460cb57f4e72cc4a73
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-04-12 19:57:41 +0300

    add auth actions

---
+++ src/client/action/authentication.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { actionTypes } from "./actionTypes";
+import setAuthToken from "../setAuthToken";
+import jwt_decode from "jwt-decode";
+
+export const loginUser = user => dispatch => {
+  axios
+    .post("/api/users/login", user)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      console.log("decoded");
+
+      console.log(decoded);
+
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      dispatch({
+        type: actionTypes.GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+export const setCurrentUser = decoded => {
+  return {
+    type: actionTypes.SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+export const logoutUser = history => dispatch => {
+  localStorage.removeItem("jwtToken");
+  setAuthToken(false);
+  dispatch(setCurrentUser({}));
+  history.push("/");
+};
