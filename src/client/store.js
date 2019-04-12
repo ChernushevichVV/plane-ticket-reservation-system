5c9063cf4fa7c6c4572745dc33da3ab629bbc2ce
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-04-12 19:57:54 +0300

    update store

--- src/client/store.js
+++ src/client/store.js
@@ -1,6 +1,7 @@
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 //import rootReducer from "./reducer/rootReducer";
 import { combineReducers } from "redux";
+import thunk from "redux-thunk";
 import * as reducers from "./reducer/index";
 
 const preloadedState = {
@@ -17,15 +18,20 @@ const preloadedState = {
     adult: 1,
     child: 0
   },
-  seats: []
+  seats: [],
+  errors: {},
+  auth: {
+    isAuthenticated: false,
+    user: {}
+  }
 };
 
 const store = createStore(
   combineReducers(reducers),
   preloadedState,
   compose(
-    //applyMiddleware(forbiddenWordsMiddleware),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
