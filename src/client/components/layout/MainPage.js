bd7ea6a41d3176b68e594cc3d503af3f69e97d47
ChernushevichVV <47659339+chernushevichvv@users.noreply.github.com>
2019-04-15 08:35:25 +0300

    transfer SignWindow call to MainPage

--- src/client/components/layout/MainPage.js
+++ src/client/components/layout/MainPage.js
@@ -4,16 +4,33 @@ import Section from "../pages/mainpage/Section";
 import Footer from "../pages/mainpage/Footer";
 import { Route, Switch } from "react-router-dom";
 import User from "../pages/accounts/User";
-
+import SignWindow from "../pages/mainpage/SignWindow";
 export class MainPage extends Component {
+  state = {
+    showLogin: false
+  };
+
+  handleClick = () => {
+    this.setState(state => ({
+      showLogin: !state.showLogin
+    }));
+  };
+
   render() {
     return (
-      <div className="container">
-        <Header />
+      <div className="mainpage">
+        <Header logInClick={this.handleClick} />
         <Switch>
           <Route path="/user" component={User} />
           <Route path="/" component={Section} />
         </Switch>
+        <SignWindow
+          show={this.state.showLogin}
+          onClick={this.handleClick}
+          title="Log into your account"
+          buttonLabel="Log in"
+          nickPlaceholder="Email or username"
+        />
         <Footer />
       </div>
     );
--- src/client/components/pages/mainpage/Header.js
+++ src/client/components/pages/mainpage/Header.js
@@ -1,45 +1,24 @@
 import React, { Component } from "react";
-import SignWindow from "./SignWindow";
 import LinkContainer from "../../misc/LinkContainer";
 class Header extends Component {
-  state = {
-    showLogin: false
-  };
-
-  handleClick = () => {
-    this.setState(state => ({
-      showLogin: !state.showLogin
-    }));
-  };
-
   render() {
+    const { logInClick } = this.props;
+
     return (
       <div className="header">
         <div className="header__text">
           <LinkContainer link="/" label="FlyInTheSky" />
         </div>
         <div className="sign-buttons-block">
-          <button
-            className="button sign-buttons-block__button"
-            onClick={this.handleClick}
-          >
-            Sign up
-          </button>
+          <button className="button sign-buttons-block__button">Sign up</button>
 
           <button
             className="button sign-buttons-block__button"
-            onClick={this.handleClick}
+            onClick={logInClick}
           >
             Log in
           </button>
         </div>
-        <SignWindow
-          show={this.state.showLogin}
-          onClick={this.handleClick}
-          title="Log into your account"
-          buttonLabel="Log in"
-          nickPlaceholder="Email or username"
-        />
       </div>
     );
   }
