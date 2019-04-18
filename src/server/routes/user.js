4203da76ed04ff8284835e9cbe75dad708ffe495
ChernushevichVV <47659339+ChernushevichVV@users.noreply.github.com>
2019-04-18 14:36:43 +0300

    add register logic to server

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -6,16 +6,47 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 
+router.post("/register", (req, res) => {
+  User.findOne({
+    name: req.body.name
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        name: "Username already exists"
+      });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) console.error("There was an error", err);
+        else {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) console.error("There was an error", err);
+            else {
+              newUser.password = hash;
+              newUser.save().then(user => {
+                res.json(user);
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
 router.post("/login", (req, res) => {
   const name = req.body.name;
   const password = req.body.password;
-
-  console.log("logging");
-  console.log(name);
+  const errors = {};
 
   User.findOne({ name }).then(user => {
     if (!user) {
-      return res.status(404).json("User not found");
+      errors.name = "User not found";
+      return res.status(404).json(errors);
     }
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
@@ -40,9 +71,9 @@ router.post("/login", (req, res) => {
             }
           }
         );
-        console.log("great!");
       } else {
-        return res.status(400).json("Incorrect Password");
+        errors.password = "Incorrect Password";
+        return res.status(400).json(errors);
       }
     });
   });
