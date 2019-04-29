cfe2770d8ba8bf2eb0ea8e6d34235dcbedde00a2
ChernushevichVV <47659339+chernushevichvv@users.noreply.github.com>
2019-04-29 10:48:33 +0300

    use material Stepper

--- src/client/components/pages/booking/BookingBar.js
+++ src/client/components/pages/booking/BookingBar.js
@@ -1,20 +1,86 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+// import Button from "@material-ui/core/Button";
+
+const styles = theme => ({
+  root: {
+    width: "90%"
+  },
+  button: {
+    marginRight: theme.spacing.unit
+  },
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit
+  }
+});
+
+function getSteps() {
+  return [
+    "Flight selection",
+    "Seat selection",
+    "Luggage",
+    "Personal data",
+    "Payment",
+    "Confirmation"
+  ];
+}
 
 export class BookingBar extends Component {
+  state = {
+    activeStep: 0
+  };
+
+  handleNext = () => {
+    const { activeStep } = this.state;
+    this.setState({
+      activeStep: activeStep + 1
+    });
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    });
+  };
+
   render() {
+    //const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
     return (
-      <div className="booking-bar border">
-        <ul className="booking-bar__list">
-          <li className="booking-bar__list-item ">Flight selection</li>
-          <li className="booking-bar__list-item">Seat selection</li>
-          <li className="booking-bar__list-item">Luggage</li>
-          <li className="booking-bar__list-item">Personal data</li>
-          <li className="booking-bar__list-item">Payment</li>
-          <li className="booking-bar__list-item">Confirmation</li>
-        </ul>
+      <div className="booking-bar">
+        {/* <div className={classes.root}> */}
+        <Stepper activeStep={activeStep} alternativeLabel={false}>
+          {steps.map(label => {
+            const props = {};
+            const labelProps = {};
+
+            return (
+              <Step key={label} {...props}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
       </div>
     );
   }
 }
 
-export default BookingBar;
+BookingBar.propTypes = {
+  classes: PropTypes.object
+};
+// export default BookingBar;
+export default withStyles(styles)(BookingBar);
