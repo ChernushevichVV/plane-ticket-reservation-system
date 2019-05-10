9576493fee337182abb85bc627958ceebe7cd5d4
ChernushevichVV <47659339+chernushevichvv@users.noreply.github.com>
2019-05-10 10:25:26 +0300

    Summary get all info from redux store

--- src/client/components/misc/Summary.js
+++ src/client/components/misc/Summary.js
@@ -1,17 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import NextButton from "./NextButton";
 import { getLink, getCurrentStep } from "../pages/booking/utilsBookingBar";
 
 const Summary = props => {
-  const { history, cost } = props;
+  const { history, date, seats, passenger, tripType } = props;
   const link = getLink(getCurrentStep(history, 1));
+  console.log("summary");
 
   return (
     <div className="summary">
-      <p>Date</p>
-      <p>Passengers</p>
-      Total cost: {cost}
+      <p>Departure date: {date.departure.toDateString()}</p>
+      {tripType === "return" && (
+        <p>Return date: {date.return.toDateString()}</p>
+      )}
+      <p>Passengers: {passenger.adult}</p>
+      {seats.length !== 0 && <p>Seats: {seats.join(", ")}</p>}
+      Total cost: 228
       <div className="summary__button">
         <NextButton link={link} />
       </div>
@@ -19,9 +25,21 @@ const Summary = props => {
   );
 };
 
-Summary.propTypes = {
-  cost: PropTypes.string,
-  history: PropTypes.object
+const mapStateToProps = state => {
+  return {
+    tripType: state.tripType,
+    date: state.date,
+    seats: state.seats,
+    passenger: state.passenger
+  };
 };
 
-export default Summary;
+Summary.propTypes = {
+  history: PropTypes.object,
+  tripType: PropTypes.string,
+  date: PropTypes.object,
+  passenger: PropTypes.object,
+  seats: PropTypes.array
+};
+
+export default connect(mapStateToProps)(Summary);
